068ad8fcc7162169e0ed0bc56a3b88d65b025afc
sebastiancaicedo <sebastiancaicedo@uninorte.edu.co>
2017-10-30 11:55:50 -0500

    first commit

---
+++ ProyectoFinal_Xamarin/ProyectoFinal_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ProyectoFinal_Xamarin;
+
+namespace ProyectoFinal_Xamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+
+    }
+}
